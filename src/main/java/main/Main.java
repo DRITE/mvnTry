169988ab73a5cdf8cc02c2fbfc92b8b50b8bcdde
DRITE <gubarevfyodor@gmail.com>
2017-06-14 19:17:35 +0300

    For first task. But with the mistake

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import MirrorRequestsServlet.MirrorRequestsServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by gubarevfn on 14.06.17.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
